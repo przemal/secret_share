eafbd9067696f2aba05288d8317b7aa0a96b5b42
Przemysław Palacz <pprzemal@gmail.com>
2019-08-03 00:30:14 +0200

    Add default static root path

--- secret_share/settings/base.py
+++ secret_share/settings/base.py
@@ -118,6 +118,7 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = str(BASE_DIR / 'static')
 STATIC_URL = '/static/'
 
 
